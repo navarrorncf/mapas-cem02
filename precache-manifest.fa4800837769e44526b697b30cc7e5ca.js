9c6c3bdae3c8e2f67b0b7cb0d9d2d01b38c36f44
Rafael Navarro C. Filho <rncf1112@gmail.com>
2020-10-13 13:22:26 -0300

    Updates

---
+++ precache-manifest.fa4800837769e44526b697b30cc7e5ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1050a6352de263f3b67ca17c9af2d2a2",
+    "url": "/mapas-cem02/index.html"
+  },
+  {
+    "revision": "acc1dba556c17da2c2ab",
+    "url": "/mapas-cem02/static/css/main.ea09f66e.chunk.css"
+  },
+  {
+    "revision": "9a4c66b813daa7f4f9b7",
+    "url": "/mapas-cem02/static/js/2.97d9b4bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mapas-cem02/static/js/2.97d9b4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acc1dba556c17da2c2ab",
+    "url": "/mapas-cem02/static/js/main.737a9a8a.chunk.js"
+  },
+  {
+    "revision": "9d05794cff82917c6184",
+    "url": "/mapas-cem02/static/js/runtime-main.1848c22f.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/mapas-cem02/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/mapas-cem02/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
