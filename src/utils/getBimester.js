b4eee48e045699566a46dcf620415bb83879be73
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-01-12 22:54:00 -0300

    changed harcoded subject names to lowercase

--- src/utils/getBimester.js
+++ src/utils/getBimester.js
@@ -1,7 +1,7 @@
 const getBimester = (group) => {
   const firstStudentGrades = group[0].grades;
 
-  const annuals = ["EDF", "LP", "MAT"];
+  const annuals = ["edf", "lp", "mat"];
   const bimesters = annuals.map(
     (subject) => firstStudentGrades[subject].length
   );
