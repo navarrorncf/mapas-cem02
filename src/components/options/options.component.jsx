17443fb42582fbb1b61b8d57ae344a1389142934
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-01-26 15:23:27 -0300

    added filters to options and connected to redux

--- src/components/options/options.component.jsx
+++ src/components/options/options.component.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setGroup, setGrade } from "../../redux/options/optionActions";
+import {
+  setGroup,
+  setGrade,
+  setFilterMode,
+} from "../../redux/options/optionActions";
 
 import { allGrades, groupsByGrade } from "../../redux/options/initialState";
 
@@ -13,6 +17,10 @@ const Options = () => {
 
   let [grade, changeGrade] = useState(
     useSelector((state) => state.options.grade)
+  );
+
+  let [filter, changeFilter] = useState(
+    useSelector((state) => state.options.filterMode)
   );
 
   const dispatch = useDispatch();
@@ -29,6 +37,12 @@ const Options = () => {
     const firstGroup = groupsByGrade[currentGrade][0];
     changeGroup(firstGroup);
     dispatch(setGroup(firstGroup));
+  };
+
+  const handleChangeFilter = (e) => {
+    const currentFilter = e.target.value;
+    changeFilter(currentFilter);
+    dispatch(setFilterMode(currentFilter));
   };
 
   return (
@@ -61,6 +75,17 @@ const Options = () => {
                 {groupName}
               </option>
             ))}
+          </select>
+        </div>
+
+        <div className="option">
+          <label htmlFor="filter">Filtro: </label>
+          <select name="filter" value={filter} onChange={handleChangeFilter}>
+            <option value="none">Nenhum</option>
+            <option value="passed">Aprovados</option>
+            <option value="ongoing">Não Aprovados</option>
+            <option value="rec">Em Recuperação</option>
+            <option value="failed">Reprovados</option>
           </select>
         </div>
       </div>
