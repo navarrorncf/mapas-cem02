311e0183e7b40c700f032c213b5944be01426a83
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-01-28 23:03:29 -0300

    added first draft of minutes card and styles

--- src/components/minute-card/minute-card.component.jsx
+++ src/components/minute-card/minute-card.component.jsx
@@ -2,29 +2,68 @@ import React from "react";
 
 import "./minute-card.styles.scss";
 
-import getFailedSubjectNames from "../../utils/getFailedSubjectNames";
 import getAttendance from "../../utils/getAttendance";
 
-const MinuteCard = ({ student }) => {
-  const failedSubjects = getFailedSubjectNames(student);
+const MinuteCard = ({ student, handleSetGroupState, handleVeredict }) => {
   const attendance = getAttendance(student);
+  const { name, code, failedSubjects } = student;
 
   return (
-    <div className="minutes-card">
-      <h3 className="name">{student.name}</h3>
+    <div className={`minute-card`}>
+      <h3 className="name">
+        {name} ({code})
+      </h3>
+      <h4>Faltas: {attendance}</h4>
+      <div
+        className="veredict"
+        onChange={(e) => handleVeredict(code, e.target.value)}
+      >
+        <div className="form-group">
+          <input
+            type="radio"
+            name={`veredict-${code}`}
+            id={`veredict-${code}-rep`}
+            value="rep"
+            defaultChecked
+          />
+          <label htmlFor={`veredict-${code}-rep`}>REP</label>
+        </div>
+        <div className="form-group">
+          <input
+            type="radio"
+            name={`veredict-${code}`}
+            id={`veredict-${code}-ap`}
+            value="ap"
+          />
+          <label htmlFor={`veredict-${code}-ap`}>AP</label>
+        </div>
+        <div className="form-group">
+          <input
+            type="radio"
+            name={`veredict-${code}`}
+            id={`veredict-${code}-apc`}
+            value="apc"
+          />
+          <label htmlFor={`veredict-${code}-apc`}>AP**</label>
+        </div>
+      </div>
       <div className="card-container">
         {failedSubjects.map((subject) => {
-          const key = `${student.code}-${subject}-card`;
+          const key = `${code}-${subject}-card`;
+
           return (
-            <div className="subject-cart">
+            <div className="subject-card" key={key}>
               <h4>{subject}</h4>
-              <select name={key} id={key} key={key}>
+              <select
+                name={key}
+                id={key}
+                defaultValue="rep"
+                onChange={(e) =>
+                  handleSetGroupState(code, subject, e.target.value)
+                }
+              >
                 <option value="ap">Aprovado</option>
-                <option value="rec-ok">Recuperado</option>
-                <option value="apc">AP**</option>
-                <option value="rep" defaultSelected>
-                  Reprovado
-                </option>
+                <option value="rep">Reprovado</option>
               </select>
             </div>
           );
