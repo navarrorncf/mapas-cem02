37ad6ca0451a4a83223c1adb86b7355b1bd164bd
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-01-31 14:41:23 -0300

    added pure function for updating state

---
+++ src/redux/options/updatePassedSubjects.js
@@ -0,0 +1,39 @@
+const updatePassedSubjects = (ongoingCases, payload) => {
+  let newOngoingCases = [...ongoingCases].map((student) => {
+    let { code, subject, status } = payload;
+
+    if (student.code === code) {
+      let newStudent = { ...student },
+        passed = newStudent.passed;
+      if (status === "ap") {
+        if (!passed) {
+          passed = [subject];
+        } else if (!passed.includes(subject)) {
+          passed.push(subject);
+        }
+        return {
+          ...newStudent,
+          passed,
+        };
+      } else if (status === "rep") {
+        if (!passed) {
+          return student;
+        } else {
+          let index = passed.findIndex((el) => el === subject);
+          passed.splice(index, 1);
+        }
+
+        return {
+          ...newStudent,
+          passed,
+        };
+      }
+    }
+
+    return student;
+  });
+
+  return newOngoingCases;
+};
+
+module.exports = updatePassedSubjects;
